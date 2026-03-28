e43724be92cf1c4f87062aeababbdad4ac0d03bf
EyadShaaban <eyad.shabaaan@gmail.com>
2026-03-28 15:22:04 -0400

    Final Task: Verified store configuration and completed Redux integration

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
+import cartReducer from './CartSlice'; // Task: Importing your cart reducer
+
+// Task: Create a Redux store using configureStore
+const store = configureStore({
     reducer: {
+        // 'cart' is the name of the slice in the store
         cart: cartReducer,
     },
 });
-export default store
+
+export default store; // Task: Exporting for use in <Provider store={store}>
